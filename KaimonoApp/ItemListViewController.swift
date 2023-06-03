296a73eeaa12cfeacc89db1bed6f8bd30bb8e994
UR27 <r.apps.biz@gmail.com>
2023-06-03 22:13:21 +0900

    ItemListViewControllerを追加しました UITabBarControllerを導入しました

---
+++ KaimonoApp/ItemListViewController.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    override func viewDidLoad() {//この画面が呼ばれた時に最初に呼ばれるブロック
+        super.viewDidLoad()
+        
+        print("ItemListViewControllerが表示されました")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
